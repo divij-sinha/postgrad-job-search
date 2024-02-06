702e9851c7fb851bc2cddfdcb0d23887dbf1f499
Kathryn Link-Oberstar <111700741+klinkoberstar@users.noreply.github.com>
2024-02-06 01:15:30 -0600

    Update search.py

--- postgrad_job_search/src/search.py
+++ postgrad_job_search/src/search.py
@@ -44,8 +44,8 @@ def launch(url_list, keywords, json_file='job_listings.json'):
             print(response.status_code)
             if response.status_code == 200:
                 soup = BeautifulSoup(response.content, 'html.parser')
+                print(soup.text)
                 for element in soup.find_all('a', href=True):
-                    print(element)
                     job_title = element.text.strip()
                     if filter_job_title(job_title) and any(keyword.lower() in job_title.lower() for keyword in keywords):
                         job_link = element['href']
