b533d96863087f5e341dd308afe6d73e678c11b9
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2024-02-13 18:34:53 -0600

    fastapi app

--- postgrad_job_search/src/app.py
+++ postgrad_job_search/src/app.py
@@ -1,60 +1,78 @@
-import streamlit as st
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse, FileResponse
+from fastapi.templating import Jinja2Templates
+import uuid
 from search import search
 import pandas as pd
 import os
+from typing import Annotated
 
-from contextlib import contextmanager, redirect_stdout
-from io import StringIO
-
-
-## https://discuss.streamlit.io/t/cannot-print-the-terminal-output-in-streamlit/6602
-@contextmanager
-def st_capture(output_func):
-    with StringIO() as stdout, redirect_stdout(stdout):
-        old_write = stdout.write
-
-        def new_write(string):
-            ret = old_write(string)
-            output_func(stdout.getvalue())
-            return ret
-
-        stdout.write = new_write
-        yield
+from fastapi import FastAPI, Form
 
 
-st.set_page_config(layout="wide")
+app = FastAPI()
 
-st.markdown("# Job Search Scraper")
+templates = Jinja2Templates(directory="templates")
 
-st.markdown("## How to use")
 
-steps = """
-1. Open the following google sheet [https://docs.google.com/spreadsheets/d/19kdQAZzyb_1xmfqrXNoZ56WYW0orUIHI0PQaP0spZQM/edit#gid=0](https://docs.google.com/spreadsheets/d/19kdQAZzyb_1xmfqrXNoZ56WYW0orUIHI0PQaP0spZQM/edit#gid=0)
-1. Go to File > Make a Copy
-1. Fill the columns shown in the sheet
-    - `Company` should be a name for you to identify the firm you are applying for
-    - `URL` should be the link to a page with jobs posted for that company
-    - `Keywords` is a list of job titles you are interested in
-    - `Exclude` is a list of other related words that you definitely do not want
-1. DO NOT CHANGE THE COLUMN NAMES
-1. You can add new columns, and change the locations of the columns, but do not change their names
-1. Once the sheet is ready, go to File > Share > Publish to Web
-1. Change "Entire Document" to the specific sheet 
-1. Change "Web page" to "Comma-separated values"
-1. Click Publish
-1. Copy the link shown and paste below  
-"""
+@app.get("/download/{file_name}", response_class=FileResponse)
+async def download_file(file_name):
+    return FileResponse(f"out/{file_name}.csv")
 
-st.write(steps)
 
-csv_link = st.text_input("Add link as specified below", os.environ["CONFIGCSV"])
-df = pd.read_csv(csv_link)
+@app.get("/")
+async def start_redirect():
+    return RedirectResponse(
+        url="/start/home",
+    )
 
-assert "Company" in df.columns
-assert "URL" in df.columns
-assert "Keywords" in df.columns
-assert "Exclude" in df.columns
 
-job_listings = search(df)
+@app.get("/start/{code}")
+async def start(request: Request, code: str = "home"):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"code": code}
+    )
 
-st.dataframe(job_listings, width=900)
+
+@app.post("/search_page")
+def call_search(request: Request, input_link: Annotated[str, Form()]):
+    if input_link == "https://secret":
+        input_link = os.environ["CONFIGCSV"]
+    df = pd.read_csv(input_link)
+    print(input_link)
+    # try:
+    assert "Company" in df.columns
+    assert "URL" in df.columns
+    assert "Keywords" in df.columns
+    assert "Exclude" in df.columns
+    job_listings = search(df)
+    file_name = uuid.uuid4()
+    file_path = f"out/{file_name}.csv"
+    job_listings.to_csv(file_path, index=False)
+    job_listings_html = job_listings.to_html(index=False, render_links=True)
+    return templates.TemplateResponse(
+        request=request,
+        name="result.html",
+        context={
+            "table_html": job_listings_html,
+            "file_name": file_name,
+        },
+    )
+
+    # except:
+    # return RedirectResponse(url="/error", status_code=status.HTTP_303_SEE_OTHER)
+
+
+# st.write(steps)
+
+#
+# df = pd.read_csv(csv_link)
+
+# assert "Company" in df.columns
+# assert "URL" in df.columns
+# assert "Keywords" in df.columns
+# assert "Exclude" in df.columns
+
+# job_listings = search(df)
+
+# st.dataframe(job_listings, width=900)
