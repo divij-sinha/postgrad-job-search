fc2f18d6395defa2229a925c28acea0539c644a1
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2024-02-15 10:12:42 -0600

    debig print

--- postgrad_job_search/src/app.py
+++ postgrad_job_search/src/app.py
@@ -46,45 +46,6 @@ def valid_df(input_link: str):
         return None
 
 
-async def call_search(df):
-    keywords = df.loc[:, "Keywords"].dropna().to_list()
-    exclude = df.loc[:, "Exclude"].dropna().to_list()
-    df = df.loc[:, ["Company", "URL"]].drop_duplicates(subset=["Company", "URL"])
-
-    N_PER_RUN = int(os.environ["N_PER_RUN"])
-    full_job_listings = []
-    links_visited = []
-    while df.shape[0] > 0:
-        print("run")
-        full_future_urls = []
-        n = df.shape[0]
-        n_runs = (n // N_PER_RUN) + 1
-        for i in range(n_runs):
-            res = await get_job_listings(
-                df.iloc[i * N_PER_RUN : min((i + 1) * N_PER_RUN, n)], keywords, exclude
-            )
-            future_urls, job_listings = res
-            full_future_urls.extend(future_urls)
-            full_job_listings.extend(job_listings)
-
-        future_df = pd.DataFrame(full_future_urls)
-        future_df = future_df.explode("URL")
-        future_df = future_df.reset_index(drop=True)
-        future_df = future_df.dropna()
-        future_df = future_df.loc[future_df.URL != "about:blank"]
-        future_df = future_df.loc[~future_df.URL.isin(links_visited)]
-        future_df = future_df.loc[~future_df.URL.str.contains("recaptcha")]
-        future_df = future_df.loc[~future_df.URL.str.contains("paypal")]
-        future_df = future_df.loc[~future_df.URL.str.contains("stripe")]
-        future_df = future_df.drop_duplicates()
-        links_visited.extend(future_df.URL)
-        df = future_df
-
-    full_job_listings = itertools.chain.from_iterable(full_job_listings)
-    full_job_listings = pd.DataFrame(full_job_listings).drop_duplicates()
-    return full_job_listings
-
-
 @app.get("/out/{file_path}")
 async def download_file(file_path):
     FileResponse(file_path)
@@ -138,4 +99,4 @@ async def websocket_endpoint(websocket: WebSocket):
         )
         await websocket.send_text(job_listings_html)
 
-        print("complete")
+        print(f"complete {full_job_listings.shape[0]}")
